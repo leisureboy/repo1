7b13bb36e1d646857e2e29e44bcaff3ffb4d113f
leisureboy <37170345+leisureboy@users.noreply.github.com>
2018-11-15 10:39:05 +0800

    添加

---
+++ maven_go/maven_service/src/main/java/cn/dzy/service/MavenService.java
@@ -0,0 +1,9 @@
+package cn.dzy.service;
+
+import cn.dzy.domain.Account;
+
+import java.util.List;
+
+public interface MavenService {
+    Account findbyid(int id);
+}
---
+++ maven_go/maven_service/src/main/java/cn/dzy/service/impl/MavenServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.dzy.service.impl;
+
+import cn.dzy.dao.MavenDao;
+import cn.dzy.domain.Account;
+import cn.dzy.service.MavenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MavenServiceImpl implements MavenService {
+    @Autowired
+    MavenDao mavenDao;
+    public Account findbyid(int id) {
+
+        return mavenDao.findbyid(id);
+    }
+
+
+
+}
