bedd78c2acd9287048c05bb67bb20cdd65dad749
leisureboy <37170345+leisureboy@users.noreply.github.com>
2018-11-15 20:35:27 +0800

    测试提交

--- maven_go/maven_dao/src/main/java/cn/dzy/dao/MavenDao.java
+++ maven_go/maven_dao/src/main/java/cn/dzy/dao/MavenDao.java
@@ -4,7 +4,7 @@ import cn.dzy.domain.Account;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-
+//测试冲突
 public interface MavenDao {
     @Select("select * from account where id = #{id}")
     Account findbyid(int id);
