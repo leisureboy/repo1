4701a85f97cfdebfd26ca51a68aef9719609fb19
leisureboy <37170345+leisureboy@users.noreply.github.com>
2018-11-15 19:29:23 +0800

    dao同步

--- maven_go/maven_dao/src/main/java/cn/dzy/dao/MavenDao.java
+++ maven_go/maven_dao/src/main/java/cn/dzy/dao/MavenDao.java
@@ -8,4 +8,9 @@ import java.util.List;
 public interface MavenDao {
     @Select("select * from account where id = #{id}")
     Account findbyid(int id);
+    //test
+    List<Account> findAll();
+    //ojbk？
+    void update();
+
 }
